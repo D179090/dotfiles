982a54db8807f09975d0ba40ce6e7fb9fe449b58
D179090 <102134977+D179090@users.noreply.github.com>
2023-06-16 22:26:02 +0300

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias config="nvim .config/fish/config.fish"
+alias calc="/home/d179090/cpp/скомпилированное/./calc"
+alias tb="nc termbin.com 9999"
+alias ls="exa --icons -l"
+
+function fish_greeting
+    #date
+end
