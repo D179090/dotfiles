d6b7e45d494facdd2360f7e92728cf9ebb5c0aaf
D179090 <102134977+D179090@users.noreply.github.com>
2023-11-19 15:39:12 +0300

    Add files via upload

--- config.fish
+++ config.fish
@@ -5,7 +5,13 @@ end
 alias config="nvim .config/fish/config.fish"
 alias tb="nc termbin.com 9999"
 alias ls="exa --icons -l"
+alias g="grep"
+alias nf="neofetch"
+alias :q="exit"
 
 function fish_greeting
     #date
 end
+
+
+source (/usr/bin/starship init fish --print-full-init | psub)
